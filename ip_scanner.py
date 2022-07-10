1917da4ff0d1da1a341e64811055924b40dd603b
Evan Mulloy <evmulloy@gmail.com>
2022-07-10 19:01:21 -0400

    Fix unused imports, formatting issues

--- ip_scanner.py
+++ ip_scanner.py
@@ -1,6 +1,8 @@
 """Scanner: A Module that contains classes for port scans and other network scans."""
-
-from scapy.all import *
+import sys
+from scapy.all import conf, sr1, sr
+from scapy.volatile import RandShort
+from scapy.sendrecv import srp
 from scapy.layers.inet import IP, TCP
 from scapy.layers.l2 import Ether, ARP
 
@@ -27,42 +29,8 @@ class Scanner:
         return hosts
 
     @staticmethod
-    def connect_scan(address, min_port, max_port, timeout=0.01):
-
-        # We will return a list of ports
-        ports = []
-
-        # Set socket timeout
-        socket.setdefaulttimeout(timeout)
-
-        # Using the range function to specify ports
-        # Also we will do error handling
-
-        try:
-            for port in range(int(min_port), int(max_port)):
-                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                result = sock.connect_ex((address, port))
-                if result == 0:
-                    print("Port {}:        Open".format(port))
-                    ports.append(port)
-                sock.close()
-
-        except KeyboardInterrupt:
-            print("You pressed Ctrl+C")
-            sys.exit()
-
-        except socket.gaierror:
-            print("Hostname could not be resolved. Exiting")
-            sys.exit()
-
-        except socket.error:
-            print("Couldn't connect to server")
-            sys.exit()
-
-        return ports
-
-    @staticmethod
     def syn_scan(dst_ip, min_port, max_port, timeout=3):
+        """Conduct a SYN scan against a destination IP from ports min_port to max_port."""
         # Set up a list of ports
         ports = []
         # Scan from a random port
@@ -70,12 +38,14 @@ class Scanner:
 
         try:
             for dst_port in range(int(min_port), int(max_port)):
-                stealth_scan_resp = sr1(IP(dst=dst_ip) / TCP(sport=src_port, dport=dst_port, flags="S"),
+                stealth_scan_resp = sr1(IP(dst=dst_ip) / TCP(sport=src_port,
+                dport=dst_port, flags="S"),
                                         timeout=timeout)
                 if not str(type(stealth_scan_resp)).__contains__("NoneType"):
                     if stealth_scan_resp.haslayer(TCP):
                         if stealth_scan_resp.getlayer(TCP).flags == 0x12:
-                            send_rst = sr(IP(dst=dst_ip) / TCP(sport=src_port, dport=dst_port, flags="R"),
+                            send_rst = sr(IP(dst=dst_ip) / TCP(sport=src_port,
+                            dport=dst_port, flags="R"),
                                           timeout=timeout)
                             ports.append(dst_port)
 
