cbe7405691e3e174864cc5980e3f7f4cdf20fe73
Evan Mulloy <evmulloy@gmail.com>
2022-07-19 22:17:15 -0400

    Implemented all scan types

--- ip_scanner.py
+++ ip_scanner.py
@@ -1,13 +1,13 @@
 """Scanner: A Module that contains classes for port scans and other network scans."""
-import sys
 from scapy.all import conf, sr1, sr
 from scapy.volatile import RandShort
 from scapy.sendrecv import srp
-from scapy.layers.inet import IP, TCP, ICMP
+from scapy.layers.inet import IP, TCP, ICMP, UDP
 from scapy.layers.l2 import Ether, ARP
 
 class ScanResult:
-    """Contains lists of ports that are either open, filtered, or open or filtered but inconclusive"""
+    """Contains lists of ports that are either open, filtered,
+    or open or filtered but inconclusive"""
 
     def __init__(self, open_ports, filtered_ports, open_or_filtered_ports):
         self.open_ports = open_ports
@@ -58,15 +58,16 @@ class Scanner:
         src_port = RandShort()
 
         for dst_port in range(int(min_port), int(max_port)):
-            stealth_scan_resp = sr1(IP(dst=dst_ip)/TCP(sport=src_port,dport=dst_port,flags="S"),timeout=timeout)
-            if(str(type(stealth_scan_resp))=="<type 'NoneType'>"):
+            stealth_scan_resp = sr1(IP(dst=dst_ip)/
+            TCP(sport=src_port,dport=dst_port,flags="S"),timeout=timeout)
+            if str(type(stealth_scan_resp))=="<type 'NoneType'>":
                 filtered_ports.append(dst_port)
-            elif(stealth_scan_resp.haslayer(TCP)):
-                if(stealth_scan_resp.getlayer(TCP).flags == 0x12):
+            elif stealth_scan_resp.haslayer(TCP):
+                if stealth_scan_resp.getlayer(TCP).flags == 0x12:
                     sr(IP(dst=dst_ip)/TCP(sport=src_port,dport=dst_port,flags="R"),timeout=timeout)
                     open_ports.append(dst_port)
-                elif(stealth_scan_resp.haslayer(ICMP)):
-                    if(int(stealth_scan_resp.getlayer(ICMP).type)==3 and
+                elif stealth_scan_resp.haslayer(ICMP):
+                    if int(stealth_scan_resp.getlayer(ICMP).type==3 and
                     int(stealth_scan_resp.getlayer(ICMP).code) in [1,2,3,9,10,13]):
                         filtered_ports.append(dst_port)
 
@@ -75,17 +76,16 @@ class Scanner:
     @staticmethod
     def xmas_scan(dst_ip, min_port, max_port, timeout=3):
         """Conduct an XMAS scan against a destination IP from ports min_port to max_port."""
-        
         filtered_ports = []
         open_or_filtered_ports = []
 
         for dst_port in range(int(min_port), int(max_port)):
             xmas_scan_resp = sr1(IP(dst=dst_ip)/TCP(dport=dst_port,flags="FPU"),timeout=timeout)
-            if (str(type(xmas_scan_resp))=="<type 'NoneType'>"):
+            if str(type(xmas_scan_resp))=="<type 'NoneType'>":
                 open_or_filtered_ports.append(dst_port)
-            elif(xmas_scan_resp.haslayer(TCP)):
-                if(xmas_scan_resp.haslayer(ICMP)):
-                    if(int(xmas_scan_resp.getlayer(ICMP).type)==3
+            elif xmas_scan_resp.haslayer(TCP):
+                if xmas_scan_resp.haslayer(ICMP):
+                    if int(xmas_scan_resp.getlayer(ICMP).type==3
                     and int(xmas_scan_resp.getlayer(ICMP).code) in [1,2,3,9,10,13]):
                         filtered_ports.append(dst_port)
 
@@ -99,62 +99,97 @@ class Scanner:
 
         for dst_port in range(int(min_port), int(max_port)):
             fin_scan_resp = sr1(IP(dst=dst_ip)/TCP(dport=dst_port,flags="F"), timeout=timeout)
-            if (str(type(fin_scan_resp))=="<type 'NoneType'>"):
+            if str(type(fin_scan_resp))=="<type 'NoneType'>":
                 open_or_filtered_ports.append(dst_port)
-            elif(fin_scan_resp.haslayer(TCP)):
-                if(fin_scan_resp.haslayer(ICMP)):
-                    if(int(fin_scan_resp.getlayer(ICMP).type)==3
+            elif fin_scan_resp.haslayer(TCP):
+                if fin_scan_resp.haslayer(ICMP):
+                    if int(fin_scan_resp.getlayer(ICMP).type==3
                         and int(fin_scan_resp.getlayer(ICMP).code) in [1,2,3,9,10,13]):
-                            filtered_ports.append(dst_port)
+                        filtered_ports.append(dst_port)
 
         return ScanResult([], filtered_ports, open_or_filtered_ports)
-    
+
     @staticmethod
     def null_scan(dst_ip, min_port, max_port, timeout=3):
-        
+        """Conduct a NULL scan against a destination IP from ports min_port to max_port."""
         open_or_filtered_ports = []
         filtered_ports = []
 
         for dst_port in range(int(min_port), int(max_port)):
             null_scan_resp = sr1(IP(dst=dst_ip)/TCP(dport=dst_port,flags=""),timeout=timeout)
-            if (str(type(null_scan_resp))=="<type 'NoneType'>"):
+            if str(type(null_scan_resp))=="<type 'NoneType'>":
                 open_or_filtered_ports.append(dst_port)
-            elif(null_scan_resp.haslayer(TCP)):
-                if(null_scan_resp.haslayer(ICMP)):
-                    if(int(null_scan_resp.getlayer(ICMP).type)==3
+            elif null_scan_resp.haslayer(TCP):
+                if null_scan_resp.haslayer(ICMP):
+                    if int(null_scan_resp.getlayer(ICMP).type==3
                         and int(null_scan_resp.getlayer(ICMP).code) in [1,2,3,9,10,13]):
-                            filtered_ports.append(dst_port)
+                        filtered_ports.append(dst_port)
 
         return ScanResult([], filtered_ports, open_or_filtered_ports)
 
     @staticmethod
     def window_scan(dst_ip, min_port, max_port, timeout=3):
-        
+        """Conduct a Window scan against a destination IP from ports min_port to max_port."""
         open_ports = []
 
         for dst_port in range(int(min_port), int(max_port)):
             window_scan_resp = sr1(IP(dst=dst_ip)/TCP(dport=dst_port,flags="A"),timeout=timeout)
-            if(window_scan_resp.haslayer(TCP)):
-                if(window_scan_resp.getlayer(TCP).window > 0):
+            if window_scan_resp.haslayer(TCP):
+                if window_scan_resp.getlayer(TCP).window > 0:
                     open_ports.append(dst_port)
 
         return ScanResult(open_ports, [], [])
 
     @staticmethod
     def udp_scan(dst_ip, min_port, max_port, timeout=3):
-        
+        """Conduct a UDP scan against a destination IP from ports min_port to max_port."""
         open_ports = []
         filtered_ports = []
         open_or_filtered_ports = []
 
-        # TODO: UDP Scan
+        for dst_port in range(int(min_port), int(max_port)):
+            udp_scan_resp = sr1(IP(dst=dst_ip)/UDP(dport=dst_port),timeout=timeout)
+            if str(type(udp_scan_resp))=="<type 'NoneType'>":
+                retrans = []
+                for count in range(0,3):
+                    retrans.append(sr1(IP(dst=dst_ip)/UDP(dport=dst_port),timeout=timeout))
+                for item in retrans:
+                    if str(type(item))!="<type 'NoneType'>":
+                        open_or_filtered_ports.append(dst_port)
+                    elif udp_scan_resp.haslayer(UDP):
+                        open_ports.append(dst_port)
+                    elif udp_scan_resp.haslayer(ICMP):
+                        if int(udp_scan_resp.getlayer(ICMP).type==3
+                            and int(udp_scan_resp.getlayer(ICMP).code) in [1,2,9,10,13]):
+                            filtered_ports.append(dst_port)
 
         return ScanResult(open_ports, filtered_ports, open_or_filtered_ports)
 
     @staticmethod
     def scan_host(scan_type, dst_ip, min_port, max_port, timeout=3):
-        ports = []
-        
-        # TODO: Change return type to scan result
-
-        return ports
+        """Conduct a port scan against a destination IP from ports min_port to max_port.
+        Keyword argments:
+        scan_type -- The scan type:
+            0/default: SYN scan
+            1: XMAS scan
+            2: FIN scan
+            3: NULL scan
+            4: Window scan
+            5: UDP scan
+        dst_ip -- The destination IP
+        min_port -- The first port in the scan range
+        max_port -- The last port in the scan range
+        timeout -- The time to wait for a response to a sent packet
+        """
+        if scan_type == 1:
+            return Scanner.xmas_scan(dst_ip, min_port, max_port, timeout)
+        elif scan_type == 2:
+            return Scanner.fin_scan(dst_ip, min_port, max_port, timeout)
+        elif scan_type == 3:
+            return Scanner.null_scan(dst_ip, min_port, max_port, timeout)
+        elif scan_type == 4:
+            return Scanner.window_scan(dst_ip, min_port, max_port, timeout)
+        elif scan_type == 5:
+            return Scanner.udp_scan(dst_ip, min_port, max_port, timeout)
+        else:
+            return Scanner.syn_scan(dst_ip, min_port, max_port, timeout)
